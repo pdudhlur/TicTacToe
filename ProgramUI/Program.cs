03307612545d49484635acdb680cbfec1bc90360
AJ Hardin <ajhardin-dev@gmail.com>
2021-12-08 18:03:48 -0500

    Adding a little bit of UI

---
+++ ProgramUI/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramUI
+{
+    // Tic Tac Toe program by Prasanth and AJ
+    internal class Program
+    {
+        public void Main(string[] args)
+        {
+            RunProgram();
+
+        }
+
+        public void RunProgram()
+        {
+            bool keepRunning = true;
+
+            while (keepRunning)
+            {
+                ShowMenu();
+
+
+            }
+
+
+            return;
+        }
+
+        public void ShowMenu()
+        {
+            Console.WriteLine("Select an option:\n" +
+                "[1] - Play");
+            string userInput = Console.ReadLine();
+
+        }
+    }
+}
